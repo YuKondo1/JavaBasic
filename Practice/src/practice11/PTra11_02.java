dbbac34e8136c857b05606601ec204c419802eea
YuKondo1 <yu.kondo.likeit@gmail.com>
2018-11-09 13:14:38 +0900

    11-2

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -6,14 +6,21 @@ package practice11;
  * Copyright(c) Rhizome Inc. All Rights Reserved.
  */
 
+import java.util.Arrays;
+
 public class PTra11_02 {
 	public static void main(String[] args) {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
-		FileReaderClass.readBookDataFile();
-		String rowArray = scanner.nextLine();
-		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
+		Book[] books = FileReaderClass.readBookDataFile();
 
+		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
+		String line = scanner.nextLine();
+
+		if(Arrays.asList(books).contains("o")) {
+			System.out.println("oo");
+		}
+		/*繰り返しで、文字の一部を一致するものを特定するメソッドを使う（探す）*/
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
 
 	}
